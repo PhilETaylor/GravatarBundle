bd931f0c184b32ce30a61aaa05eb541993b3777b
Phil E. Taylor <phil@phil-taylor.com>
2022-01-08 15:05:52 +0000

    Update OrnicarGravatarBundle.php

--- OrnicarGravatarBundle.php
+++ OrnicarGravatarBundle.php
@@ -6,12 +6,12 @@ use Symfony\Component\HttpKernel\Bundle\Bundle as BaseBundle;
 
 class OrnicarGravatarBundle extends BaseBundle
 {
-    public function getNamespace()
+    public function getNamespace(): string
     {
         return __NAMESPACE__;
     }
 
-    public function getPath()
+    public function getPath(): string
     {
         return __DIR__;
     }
