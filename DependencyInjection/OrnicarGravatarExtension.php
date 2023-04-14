3c1c7a309a7150bc1ee55a4f15cbe6a827ba0743
Phil E. Taylor <phil@phil-taylor.com>
2023-04-14 11:55:26 +0100

    Update OrnicarGravatarExtension.php

--- DependencyInjection/OrnicarGravatarExtension.php
+++ DependencyInjection/OrnicarGravatarExtension.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class OrnicarGravatarExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $loader = new XmlFileLoader($container, new FileLocator(array(__DIR__.'/../Resources/config')));
         $loader->load('config.xml');
