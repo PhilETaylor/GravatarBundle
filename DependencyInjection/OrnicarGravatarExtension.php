5a61460ab5b735771a21551d524566ee1601b419
Phil E. Taylor <phil@phil-taylor.com>
2024-11-25 21:09:40 +0000

    Update OrnicarGravatarExtension.php

--- DependencyInjection/OrnicarGravatarExtension.php
+++ DependencyInjection/OrnicarGravatarExtension.php
@@ -6,7 +6,7 @@ use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class OrnicarGravatarExtension extends Extension
 {
