e26b7f89779f979af6436e7a879562922f979986
Phil E. Taylor <phil@phil-taylor.com>
2026-01-09 18:30:06 +0000

    Update for Symfony 8.0 compatibility - convert XML to PHP config

--- DependencyInjection/OrnicarGravatarExtension.php
+++ DependencyInjection/OrnicarGravatarExtension.php
@@ -5,15 +5,15 @@ namespace Ornicar\GravatarBundle\DependencyInjection;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class OrnicarGravatarExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new XmlFileLoader($container, new FileLocator(array(__DIR__.'/../Resources/config')));
-        $loader->load('config.xml');
+        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.php');
 
         $configuration = new Configuration();
         $processor = new Processor();
